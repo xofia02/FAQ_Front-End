c71503b9cc9593f61f4530859e314285bcff1be3
xofia02 <sofi02santos@gmail.com>
2025-06-09 19:30:03 -0300

    home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,34 @@
-
 import './Style.css';
-
 
 function Home() {
   return (
-   <><>
-    <h1>oi</h1>
-   </></>   
-    
-     )
+    <div className="home-container">
+
+      {/* Bloco 1: Barra de pesquisa com título */}
+      <div className="search-section">
+        <h1>Bem-vindo ao FAQ</h1>
+        <input
+          type="text"
+          placeholder="Pesquise uma pergunta..."
+          className="search-input"
+        />
+      </div>
+
+      {/* Bloco 2: Container grande para os cards */}
+      <div className="card-box">
+        {/* Bloco 3: Os 6 cards */}
+        <div className="card-grid">
+          <div className="card">Pergunta 1<br /><small>Resposta curta 1</small></div>
+          <div className="card">Pergunta 2<br /><small>Resposta curta 2</small></div>
+          <div className="card">Pergunta 3<br /><small>Resposta curta 3</small></div>
+          <div className="card">Pergunta 4<br /><small>Resposta curta 4</small></div>
+          <div className="card">Pergunta 5<br /><small>Resposta curta 5</small></div>
+          <div className="card">Pergunta 6<br /><small>Resposta curta 6</small></div>
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default Home;
-
